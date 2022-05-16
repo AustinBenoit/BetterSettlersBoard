512cc8f6c47b93b2d3aaff279d46d011a54dcebe
Austin Benoit <22805659+AustinBenoit@users.noreply.github.com>
2022-05-16 19:07:31 -0400

    Feat: competed the code to print the board.

--- README.md
+++ README.md
@@ -5,3 +5,41 @@ The goal of the project is to create a "digital" version of the hexagons used in
 Catan is fun game but the setup can take quite a while. So I wish to have a set of digital hexagons
 that I can simply click together in any order and have the game board ready to go.
 
+## How To's
+Currently all it can do is print out the suggested starting board from the catan instructions.
+The following is the output of the command line.
+The plan is for this to be used during the development process.
+When reading a tile the first number is the asscociated dice number while the next two characters are the type of resource. 
+The resources are
+```
+WH = wheat
+WO = wood
+CL = clay
+IR = iron
+SH = sheep
+DE = desert
+```
+
+```
+              ____
+             /    \
+        ____/ 9  WH\____
+       /    \      /    \
+  ____/ 8  WO\____/12  WH\____
+ /    \      /    \      /    \
+/ 5  CL\____/11  WO\____/10  IR\
+\      /    \      /    \      /
+ \____/ 3  IR\____/ 6  CL\____/
+ /    \      /    \      /    \
+/ 6  WH\____/ 0  DE\____/ 2  SH\
+\      /    \      /    \      /
+ \____/ 4  WH\____/ 4  SH\____/
+ /    \      /    \      /    \
+/11  SH\____/ 3  WO\____/ 9  WO\
+\      /    \      /    \      /
+ \____/ 5  SH\____/10  CL\____/
+      \      /    \      /
+       \____/ 8  IR\____/
+            \      /
+             \____/
+			                     ```
--- code/boardGenerator/boardGenerator.cpp
+++ code/boardGenerator/boardGenerator.cpp
@@ -9,25 +9,25 @@ Board::Board(std::array<std::pair<TileType, int>, 19> board) {
 }
 
 Board::Board() {
-  board[0] = std::make_pair(TileType::wood, 1);
-  board[1] = std::make_pair(TileType::wood, 1);
-  board[2] = std::make_pair(TileType::wood, 1);
-  board[3] = std::make_pair(TileType::wood, 1);
-  board[4] = std::make_pair(TileType::wood, 1);
-  board[5] = std::make_pair(TileType::wood, 1);
-  board[6] = std::make_pair(TileType::wood, 1);
-  board[7] = std::make_pair(TileType::wood, 1);
-  board[8] = std::make_pair(TileType::wood, 1);
-  board[9] = std::make_pair(TileType::wood, 1);
-  board[10] = std::make_pair(TileType::wood, 1);
-  board[11] = std::make_pair(TileType::wood, 1);
-  board[12] = std::make_pair(TileType::wood, 1);
-  board[13] = std::make_pair(TileType::wood, 1);
-  board[14] = std::make_pair(TileType::wood, 1);
-  board[15] = std::make_pair(TileType::wood, 1);
-  board[16] = std::make_pair(TileType::wood, 1);
-  board[17] = std::make_pair(TileType::wood, 1);
-  board[18] = std::make_pair(TileType::wood, 1);
+  board[0] = std::make_pair(TileType::wheat, 9);
+  board[1] = std::make_pair(TileType::wood, 8);
+  board[2] = std::make_pair(TileType::wheat, 12);
+  board[3] = std::make_pair(TileType::clay, 5);
+  board[4] = std::make_pair(TileType::wood, 11);
+  board[5] = std::make_pair(TileType::iron, 10);
+  board[6] = std::make_pair(TileType::iron, 3);
+  board[7] = std::make_pair(TileType::clay, 6);
+  board[8] = std::make_pair(TileType::wheat, 6);
+  board[9] = std::make_pair(TileType::desert, 00);
+  board[10] = std::make_pair(TileType::sheep, 2);
+  board[11] = std::make_pair(TileType::wheat, 4);
+  board[12] = std::make_pair(TileType::sheep, 4);
+  board[13] = std::make_pair(TileType::sheep, 11);
+  board[14] = std::make_pair(TileType::wood, 3);
+  board[15] = std::make_pair(TileType::wood, 9);
+  board[16] = std::make_pair(TileType::sheep, 5);
+  board[17] = std::make_pair(TileType::clay, 10);
+  board[18] = std::make_pair(TileType::iron, 8);
 }
 
 void Board::setTile(int tile, std::pair<TileType, int>tile_data) {
@@ -37,7 +37,7 @@ void Board::setTile(int tile, std::pair<TileType, int>tile_data) {
 std::pair<TileType, int> Board::getTile(int tile) { return board[tile]; }
 
 void Board::printBoard(){
-  const char* ascii_board =
+  const char *ascii_board =
 R"(              ____
              /    \
         ____/%2d  %s\____
@@ -48,17 +48,18 @@ R"(              ____
 \      /    \      /    \      /
  \____/%2d  %s\____/%2d  %s\____/
  /    \      /    \      /    \
-/      \____/      \____/      \
+/%2d  %s\____/%2d  %s\____/%2d  %s\
 \      /    \      /    \      /
- \____/      \____/      \____/
+ \____/%2d  %s\____/%2d  %s\____/
  /    \      /    \      /    \
-/      \____/      \____/      \
+/%2d  %s\____/%2d  %s\____/%2d  %s\
 \      /    \      /    \      /
- \____/      \____/      \____/
+ \____/%2d  %s\____/%2d  %s\____/
       \      /    \      /
-       \____/      \____/
+       \____/%2d  %s\____/
             \      /
-             \____/)";
+             \____/
+)";
 
   // need to flip these around
   printf(ascii_board,
@@ -69,6 +70,17 @@ R"(              ____
 	 board[4].second, tileTypeEnumNames[board[4].first],
 	 board[5].second, tileTypeEnumNames[board[5].first],
 	 board[6].second, tileTypeEnumNames[board[6].first],
-	 board[7].second, tileTypeEnumNames[board[7].first]
+	 board[7].second, tileTypeEnumNames[board[7].first],
+	 board[8].second, tileTypeEnumNames[board[8].first],
+	 board[9].second, tileTypeEnumNames[board[9].first],
+	 board[10].second, tileTypeEnumNames[board[10].first],
+	 board[11].second, tileTypeEnumNames[board[11].first],
+	 board[12].second, tileTypeEnumNames[board[12].first],
+	 board[13].second, tileTypeEnumNames[board[13].first],
+	 board[14].second, tileTypeEnumNames[board[14].first],
+	 board[15].second, tileTypeEnumNames[board[15].first],
+	 board[16].second, tileTypeEnumNames[board[16].first],
+	 board[17].second, tileTypeEnumNames[board[17].first],
+	 board[18].second, tileTypeEnumNames[board[18].first]
 	 );
  }
